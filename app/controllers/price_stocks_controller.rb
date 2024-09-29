d6688b4347c13d4a08ae1c67773d6352ad3fc561
maskrebel <33626739+maskrebel@users.noreply.github.com>
2024-09-29 19:59:28 +0700

    feat: create controller price_stock

---
+++ app/controllers/price_stocks_controller.rb
@@ -0,0 +1,37 @@
+class PriceStocksController < ApplicationController
+  def show
+    ticker = params[:ticker]
+
+    begin
+      stock_price = LatestStockPrice::Client.price(ticker)
+      render json: { ticker: ticker, price: stock_price }, status: :ok and return
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    tickers = params[:tickers]&.split(',')
+    puts("gifari")
+    puts(params[:tickers])
+    if tickers.present?
+      begin
+        prices = LatestStockPrice::Client.prices(params[:tickers])
+        render json: { tickers: tickers, prices: prices }, status: :ok
+      rescue => e
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'Tickers parameter is required' }, status: :bad_request
+    end
+  end
+
+  def all
+    begin
+      all_prices = LatestStockPrice::Client.price_all
+      render json: all_prices, status: :ok
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
+  end
+end
